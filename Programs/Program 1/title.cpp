fc1d01fc6e993778fa38087b4d90c3f08630072a
Justin Kirk <thejuki@hotmail.com>
2015-08-28 15:33:33 -0500

    Stubs for title cpp

--- Programs/Program 1/title.cpp
+++ Programs/Program 1/title.cpp
@@ -5,3 +5,35 @@ Due Date   : null
 Author     : Justin Kirk
 Description: Code for title data
 */
+
+#include "title.h"
+
+bool PrimaryIndex::set_title_key(std::string my_title, int my_key)
+{
+    // Stub
+}
+
+void PrimaryIndex::writePrimary()
+{
+    // Stub
+}
+
+void PrimaryIndex::readPrimary()
+{
+    // Stub
+}
+
+void PrimaryIndex::outputKey()
+{
+    // Stub
+}
+
+void PrimaryIndex::change_title(std::string new_title, int key)
+{
+    // Stub
+}
+
+void PrimaryIndex::matchTitle(std::string inTitle)
+{
+    // Stub
+}
