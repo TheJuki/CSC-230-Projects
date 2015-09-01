5a6ea161c6ab8af75cb7366e29b758e9a123e131
Justin Kirk <thejuki@hotmail.com>
2015-09-01 15:26:07 -0500

    BimaryFileExample - Does it work?

--- BinaryFileExample/main.cpp
+++ BinaryFileExample/main.cpp
@@ -68,7 +68,7 @@ bool fileExists(char* filename)
 
 long fileSize(char* fileName)
 {
-    ifstream tryIt(filename, ios::in);
+    ifstream tryIt(fileName, ios::in);
 
     //check to see if the file exists
 
@@ -80,7 +80,7 @@ long fileSize(char* fileName)
     {
         tryIt.seekg(0, ios::end);
 
-        long answer = tryIt.tellg;
+        long answer = tryIt.tellg();
         tryIt.close();
         return answer;
     }
@@ -88,5 +88,107 @@ long fileSize(char* fileName)
 
 int main(void)
 {
-    char
+    char outputFileName[80];
+    int pos = 0;
+    do
+    {
+        cout << "Enter a name (not now in use) for an output file : ";
+        cin >> outputFileName;
+
+        if(fileExists(outputFileName))
+        {
+            cout << "A file by that name already exists! Try again."  << endl;
+        }
+    } while (fileExists(outputFileName));
+
+    // Available access modes:
+    // ios::in     - open file for reading
+    // ios::out    - open file for writing
+    // ios::trunc  - truncate the file to have size 0 before reading/writing
+    // ios::binary - open file in binary mode (not text mode)
+
+    fstream outputFile(outputFileName, ios::out | ios::binary);
+
+    if(!outputFile)
+    {
+        cout << "Unable to open '" << outputFileName << "' for output." << endl;
+        return(1);
+    }
+    else
+    {
+        cout << "Output file correctly opened." << endl;
+    }
+
+    // Create a MyClass object using default constructor
+    MyClass myObject;
+    // write out 14 records using the same MyClass object
+    for (int i = 1; i < 15; i++)
+    {
+        // modify Object
+        myObject.set(i, i * 2.0f);
+
+        myObject.writeIt(outputFile, pos);
+        pos ++;
+    }
+
+    outputFile.close();
+
+    if(!outputFile)
+    {
+        cout << "Unable to open '" << outputFile << "' for re-reading." << endl;
+        return(1);
+    }
+    else
+    {
+        cout << "File correctly re-opened." << endl;
+    }
+
+    // Read and echo entire file
+    pos = 0;
+    myObject.readIt(outputFile, pos);
+    while(!outputFile.eof())
+    {
+        cout << myObject;
+        pos++;
+        myObject.readIt(outputFile, pos);
+    }
+    outputFile.close();
+
+    //Mess with direct access
+    outputFile.open(outputFileName, ios::in | ios::binary);
+    long inputFileSize = fileSize(outputFileName);
+
+    cout << "Size of file in bytes      = " << inputFileSize << endl;
+    cout << "Bytes per record (Object) = " << sizeof(myObject)
+         << " (an int and a float)." << endl;
+    cout << "Number of record in file = " << inputFileSize / sizeof(myObject)
+         << endl;
+
+    if(inputFileSize % sizeof(myObject) != 0)
+    {
+        cout << "Something is funny about that file size!!!" << endl;
+    }
+
+    myObject.readIt(outputFile, 2);
+    cout << "Record 3 holds: " << myObject;
+
+    myObject.readIt(outputFile, 8);
+    cout << "Record 9 holds: " << myObject;
+
+    myObject.readIt(outputFile, 7);
+    cout << "Record 8 holds: " << myObject;
+    outputFile.close();
+
+    outputFile.open(outputFileName, ios::in | ios::out | ios::binary);
+    cout << "We will now do direct access to update record 8 in that file.\n";
+
+    myObject.set(88, 3.14f);
+    myObject.writeIt(outputFile, 7);
+    myObject.readIt(outputFile, 7);
+
+    cout << "Record 8 now holds " << myObject;
+    cout << "Above line should show 88 and 3.14." << endl;
+    outputFile.close();
+
+
 }
