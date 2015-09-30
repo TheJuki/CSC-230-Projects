d794bd07e8c770ecf0be5d45179147a982ff3401
Justin Kirk <thejuki@hotmail.com>
2015-09-30 17:31:55 -0500

    Lab 4 start

--- Labs/Lab 4/main.cpp
+++ Labs/Lab 4/main.cpp
@@ -4,6 +4,19 @@ using namespace std;
 
 int main()
 {
-    string array[][][];
+    int rows = 73;
+    int columns = 3;
+    int array[rows][columns];
+
+    for (int r = 0; r < rows; ++r)
+    {
+        for (int c = 0; c < columns; ++c)
+        {
+          array[r][c]= 0;
+          //cout << array[r][c] << endl;
+        }
+    }
+
+
     return 0;
 }
