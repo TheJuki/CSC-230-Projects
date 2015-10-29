cecffe9ff98945cfe8a3dfd2ad46d75579689028
Justin Kirk <thejuki@hotmail.com>
2015-10-28 19:55:35 -0500

    Remove extra code to handle deleting a record.

--- Programs/Program 2/artist.cpp
+++ Programs/Program 2/artist.cpp
@@ -86,19 +86,6 @@ bool ArtistIndex::deleteArtist(std::string inArtist, int pos)
                 break;
             } //end if
         } // end for
-
-        //Readjust list
-        if(ArtistIndex::my_list[location].pos[0] != 0)
-        {
-             for(int k = 1; k <= ArtistIndex::my_list[location].pos[0]; ++k)
-            {
-                if(ArtistIndex::my_list[location].pos[k] == 0)
-                {
-                    ArtistIndex::my_list[location].pos[k] = ArtistIndex::my_list[location].pos[k+1];
-                }
-            } // end for
-        } // end if
-
     } // end if
     return foundArtist;
 } // end deleteArtist
